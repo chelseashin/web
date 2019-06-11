ea33e1d52ca33660ebc900db1b2409710413ddff
chelseashin <chaewonshin95@gmail.com>
2019-06-12 01:35:30 +0900

    190612 | JavaScript study

--- javascript/colors.js
+++ javascript/colors.js
@@ -1,21 +1,26 @@
 //   객체화 - Links, Body
 var Links = {
     setColor: function (color){
-        var alist = document.querySelectorAll('a');
-        var i = 0;
-        while(i < alist.length){
-        alist[i].style.color = color;
-        i = i + 1;
-        }
+        // var alist = document.querySelectorAll('a');
+        // var i = 0;
+        // while(i < alist.length){
+        // alist[i].style.color = color;
+        // i = i + 1;
+        // }
+        // 모든 a태그를 jQuery로 제어하겠다는 뜻 - 위와 같은 기능을 함
+        $('a').css('color', color);
     }
 }
 var Body = {
 setColor: function (color){
-    document.querySelector('body').style.color = color;
+    // document.querySelector('body').style.color = color;
+    // jQuery로 표현
+    $('body').css('color', color);
 },
 setBackgroundColor: function (color) {
-    document.querySelector('body').style.backgroundColor = color;
-}
+    // document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor', color);
+    }
 }
 function nightDayHandler(self){
 var target = document.querySelector('body');
